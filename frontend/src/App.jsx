e1e8da3fff272c4fc7a5020cd892d0fe2696e1bf
Rugz007 <dsweb007@gmail.com>
2022-02-23 09:55:34 +0530

    Dockerized full application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Button } from "antd";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -7,7 +8,11 @@ function App() {
       <header className="App-header">
         <img src={"ai4bharat.png"} alt="logo" />
         <p>Shoonya by AI4Bharat</p>
-        <Button size='large' type="primary">Login</Button>
+        <Link to='/login'>
+          <Button size="large" type="primary">
+            Login
+          </Button>
+        </Link>
       </header>
     </div>
   );
