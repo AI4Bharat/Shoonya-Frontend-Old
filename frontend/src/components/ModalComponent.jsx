c181f715a11f331dc4c8630189d425806dc8d341
aavaig-malhotra <aavaigmalhotra2001@gmail.com>
2022-03-28 13:19:03 +0530

    update : remove logs and comments

--- frontend/src/components/ModalComponent.jsx
+++ frontend/src/components/ModalComponent.jsx
@@ -1,47 +1,45 @@
-import React from 'react';
-import { Modal, Button } from 'antd';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Modal, Button } from "antd";
+import PropTypes from "prop-types";
 
-const ModalComponent = ({isOpenModal, setIsOpenModal,title,formSubmit, children}) => {
-//   const [isModalVisible, setIsModalVisible] = useState(false);
-
-//   const showModal = () => {
-//     setIsModalVisible(true);
-//   };
-
+const ModalComponent = ({
+  isOpenModal,
+  setIsOpenModal,
+  title,
+  formSubmit,
+  children,
+}) => {
   const handleOk = () => {
-    // setIsModalVisible(false);
-    // console.log('Here');
     formSubmit();
-    // setIsOpenModal(false);
   };
 
   const handleCancel = () => {
-    // setIsModalVisible(false);
     setIsOpenModal(false);
   };
 
-  if(!isOpenModal) return null;
+  if (!isOpenModal) return null;
 
   return (
     <>
-      {/* <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button> */}
-      <Modal title={title} visible={isOpenModal} onOk={handleOk} footer={null} onCancel = {handleCancel}>
-       {children}
-       {/* <h1>Hello</h1> */}
+      <Modal
+        title={title}
+        visible={isOpenModal}
+        onOk={handleOk}
+        footer={null}
+        onCancel={handleCancel}
+      >
+        {children}
       </Modal>
     </>
   );
 };
 
 ModalComponent.propTypes = {
-    isOpenModal: PropTypes.bool,
-    setIsOpenModal: PropTypes.func,
-    children : PropTypes.element,
-    title : PropTypes.string,
-    formSubmit : PropTypes.func,
-}
+  isOpenModal: PropTypes.bool,
+  setIsOpenModal: PropTypes.func,
+  children: PropTypes.element,
+  title: PropTypes.string,
+  formSubmit: PropTypes.func,
+};
 
 export default ModalComponent;
--- frontend/src/utils/apiInstance.js
+++ frontend/src/utils/apiInstance.js
@@ -1,88 +1,85 @@
-import axios from 'axios';
-import { apiData } from './apiData';
+import axios from "axios";
+import { apiData } from "./apiData";
 
-const ACCESS_TOKEN = 'shoonya_access_token';
-const REFRESH_TOKEN = 'shoonya_refresh_token';
-const TOKEN_NOT_VALID = 'token_not_valid';
+const ACCESS_TOKEN = "shoonya_access_token";
+const REFRESH_TOKEN = "shoonya_refresh_token";
+const TOKEN_NOT_VALID = "token_not_valid";
 
-const REFRESH_URL = 'users/auth/jwt/refresh';
+const REFRESH_URL = "users/auth/jwt/refresh";
 
 const axiosInstance = axios.create({
-    baseURL: apiData.url,
-    timeout: 500000,
-    headers: {
-        Authorization: localStorage.getItem(ACCESS_TOKEN)
-            ? 'JWT ' + localStorage.getItem(ACCESS_TOKEN)
-            : null,
-        'Content-Type': 'application/json',
-        accept: 'application/json',
-    }
+  baseURL: apiData.url,
+  timeout: 500000,
+  headers: {
+    Authorization: localStorage.getItem(ACCESS_TOKEN)
+      ? "JWT " + localStorage.getItem(ACCESS_TOKEN)
+      : null,
+    "Content-Type": "application/json",
+    accept: "application/json",
+  },
 });
 
-axiosInstance.interceptors.response.use((response) => {
-        return response;
-    },
-    function (error) {
-        const originalRequest = error.config;
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    const originalRequest = error.config;
 
-        console.log(error);
-        if (typeof error.response === 'undefined') {
-            console.log('Here');
-            alert(
-                'Unknown server error!'
-            );
-            return Promise.reject(error);
-        }
-
-        if  (error.response.status === 401 && originalRequest.url === apiData.url + REFRESH_URL) {
-            console.log('Here');
-            window.location.href = '/login';
-            return Promise.reject(error);
-        }
-
-        if (error.response.data.code === TOKEN_NOT_VALID && error.response.status === 401 && error.response.statusText === 'Unauthorized') {
-            console.log('Here');
-            const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-
-            if (refreshToken) {
-                console.log('Here');
-                const tokenParts = JSON.parse(atob(refreshToken.split('.')[1]));
-                const now = Math.ceil(Date.now() / 1000);
-
-                if (tokenParts.exp > now) {
-                    console.log('Here');
-                    return axiosInstance
-                            .post(REFRESH_URL, {
-                                refresh: refreshToken,
-                            })
-                            .then((response) => {
-                                console.log(response.data)
-                                localStorage.setItem(ACCESS_TOKEN, response.data.access);
-                                localStorage.setItem(REFRESH_TOKEN, response.data.refresh);
-
-                                axiosInstance.defaults.headers['Authorization'] =
-                                    'JWT ' + response.data.access;
-                                    originalRequest.headers['Authorization'] =
-                                    'JWT ' + response.data.access;
-
-                                return axiosInstance(originalRequest);
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                            });
-                } else {
-                    console.log('Here');
-                    window.location.href = '/';
-                }
-            } else {
-                console.log('Here');
-                window.location.href = '/';
-            }
-
-            console.log('Here');
-            return Promise.reject(error);
-        }
+    if (typeof error.response === "undefined") {
+      alert("Unknown server error!");
+      return Promise.reject(error);
     }
+
+    if (
+      error.response.status === 401 &&
+      originalRequest.url === apiData.url + REFRESH_URL
+    ) {
+      window.location.href = "/login";
+      return Promise.reject(error);
+    }
+
+    if (
+      error.response.data.code === TOKEN_NOT_VALID &&
+      error.response.status === 401 &&
+      error.response.statusText === "Unauthorized"
+    ) {
+      const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+
+      if (refreshToken) {
+        const tokenParts = JSON.parse(atob(refreshToken.split(".")[1]));
+        const now = Math.ceil(Date.now() / 1000);
+
+        if (tokenParts.exp > now) {
+          return axiosInstance
+            .post(REFRESH_URL, {
+              refresh: refreshToken,
+            })
+            .then((response) => {
+              console.log(response.data);
+              localStorage.setItem(ACCESS_TOKEN, response.data.access);
+              localStorage.setItem(REFRESH_TOKEN, response.data.refresh);
+
+              axiosInstance.defaults.headers["Authorization"] =
+                "JWT " + response.data.access;
+              originalRequest.headers["Authorization"] =
+                "JWT " + response.data.access;
+
+              return axiosInstance(originalRequest);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else {
+          window.location.href = "/";
+        }
+      } else {
+        window.location.href = "/";
+      }
+
+      return Promise.reject(error);
+    }
+  }
 );
 
 export default axiosInstance;
