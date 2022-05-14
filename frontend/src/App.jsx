f7b2ba8aeef826cefb08fe8d1faad4c332847a67
Atharva Naphade <naphade21@gmail.com>
2022-05-14 08:15:12 +0530

    login redirect bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,21 @@ import { Link } from "react-router-dom";
 import {React} from "react";
 
 function App() {
+  const handleRedirect = () => {
+    if (localStorage.getItem('shoonya_access_token')) {
+      window.location.pathname = "/dashboard";
+    } else {
+      window.location.pathname = "/login";
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={"ai4bharat.png"} alt="logo" />
         <p>Shoonya by AI4Bharat</p>
-        <Link to='/login'>
-          <Button size="large" type="primary">
+        <Link to="#">
+          <Button size="large" type="primary" onClick={handleRedirect}>
             Login
           </Button>
         </Link>
