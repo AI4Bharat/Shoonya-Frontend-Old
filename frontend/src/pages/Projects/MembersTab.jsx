12bede4e8f6383854429d95f640fc3a4e1350c4c
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2022-05-24 14:56:59 +0530

    button not visible to annotators

--- frontend/src/pages/Projects/MembersTab.jsx
+++ frontend/src/pages/Projects/MembersTab.jsx
@@ -1,16 +1,18 @@
 import { Table, Button, Modal, Select } from "antd";
 import Title from "antd/lib/typography/Title";
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { addAnnotatorsToProject } from "../../api/ProjectAPI";
+import UserContext from "../../context/User/UserContext";
 import { memberColumns } from "./TasksTableContent";
 
 export function MembersTab({ projectMembers }) {
 	const [modalOpen, setModalOpen] = useState(false);
 	const [addedUsers, setAddedUsers] = useState([]);
 	const { project_id: projectId } = useParams();
+	const userContext = useContext(UserContext);
 
 	const addNewUsers = async () => {
 		await addAnnotatorsToProject(projectId, addedUsers).then(() =>
@@ -21,26 +23,30 @@ export function MembersTab({ projectMembers }) {
 
 	return (
 		<>
-			<Button
-				style={{ width: "100%", marginBottom: "1%" }}
-				type="primary"
-				onClick={() => setModalOpen(true)}
-			>
-				Add Users to Project
-			</Button>
-			<Modal
-				visible={modalOpen}
-				onCancel={() => setModalOpen(false)}
-				onOk={() => addNewUsers()}
-			>
-				<Title level={5}>Enter the emails to be added</Title>
-				<Select
-					mode="tags"
-					style={{ width: "100%", marginTop: "5%" }}
-					onChange={(users) => setAddedUsers(users)}
-					value={addedUsers}
-				/>
-			</Modal>
+			{(userContext.user?.role === 2 || userContext.user?.role === 3) && (
+				<>
+					<Button
+						style={{ width: "100%", marginBottom: "1%" }}
+						type="primary"
+						onClick={() => setModalOpen(true)}
+					>
+						Add Users to Project
+					</Button>
+					<Modal
+						visible={modalOpen}
+						onCancel={() => setModalOpen(false)}
+						onOk={() => addNewUsers()}
+					>
+						<Title level={5}>Enter the emails to be added</Title>
+						<Select
+							mode="tags"
+							style={{ width: "100%", marginTop: "5%" }}
+							onChange={(users) => setAddedUsers(users)}
+							value={addedUsers}
+						/>
+					</Modal>
+				</>
+			)}
 			<Table columns={memberColumns} dataSource={projectMembers} />
 		</>
 	);
