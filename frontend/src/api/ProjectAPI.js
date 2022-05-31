1edbbbe910a984e7bbe448f6af89d9a2bd51a8a8
Ishvinder Sethi <ishvindersethi22@gmail.com>
2022-05-31 18:46:34 +0530

    Revert "Re-push required changes from #PR91"

--- frontend/src/api/ProjectAPI.js
+++ frontend/src/api/ProjectAPI.js
@@ -90,19 +90,22 @@ const exportProject = async (id) => {
   try {
     let response = await axiosInstance.post(`/projects/${id}/project_export/`);
 
-    if (response.status !== 200){
+    if (response.status !== 200)
       return message.error("Unable to Export Project");
-    }
-      message.success("Successfully Exported Project");
-      return;
-    } catch (error) {
-      message.error(error);
-    }
+
+    if (response.data.message === "This project is Exported")
+      message.success("This Project is Exported");
+    else message.success("This Project has already been Exported");
+
+    return;
+  } catch (error) {
+    message.error(error);
+  }
 };
 const PullNewData = async (id) => {
   try {
     let response = await axiosInstance.post(`/projects/${id}/pull_new_items/` );
-console.log(response)
+
     if (response.status !== 200)
       return message.error("Unable to pull New Items");
 
@@ -115,21 +118,6 @@ console.log(response)
     message.error(error);
   }
 };
-const downloadProject = async (id) => {
-
-  try {
-    let response = await axiosInstance.get(`/projects/${id}/download?export_type=CSV`);
-    console.log(response,"data")
-    if (response.status !== 200){
-      return message.error("Unable to Download Project");
-    }
-      message.success("Successfully Downloaded Project");
-     
-      return response;
-    } catch (error) {
-      message.error(error);
-    }
-};
 
 export {
   fetchProjects,
@@ -140,5 +128,4 @@ export {
   getProjectMembers ,
   exportProject,
   PullNewData,
-  downloadProject,
 };
--- frontend/src/pages/Projects/ProjectSettings.jsx
+++ frontend/src/pages/Projects/ProjectSettings.jsx
@@ -1,33 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { Button, Col, Form, Input, Row, Card, Space } from "antd";
+import { Button, Col, Form, Input, Row, Card } from "antd";
 import Title from "antd/lib/typography/Title";
 import { useParams, useNavigate, Link } from "react-router-dom";
+import { snakeToTitleCase } from "../../utils/stringConversions";
 import useFullPageLoader from "../../hooks/useFullPageLoader";
 import {
-  addAnnotatorsToProject,
   publishProject,
   getProject,
   updateProject,
   exportProject,
   PullNewData,
-  downloadProject,
 } from "../../api/ProjectAPI";
-import { CSVDownload } from "react-csv";
-import { Select } from 'antd';
-
 
 function ProjectSettings() {
   const { id } = useParams();
   let navigate = useNavigate();
-  const { TextArea } = Input;
-  const { Option } = Select;
+
   const [basicSettingsForm] = Form.useForm();
-  const [data, setData] = useState(true);
+
   const [isLoading, setLoading] = useState(true);
   const [project, setProject] = useState({});
   const [published, setPublished] = useState(false);
   const [loader, showLoader, hideLoader] = useFullPageLoader();
-  const [options, setOptions] = useState("CSV");
 
   const prefilBasicForm = () => {
     basicSettingsForm.setFieldsValue({
@@ -42,57 +36,19 @@ function ProjectSettings() {
       setLoading(false);
     });
   }, []);
-  const onFinishAddAnnotator = async (values) => {
-    const emails = values.emails.split(",").map((email) => email.trim());
 
-    await addAnnotatorsToProject(id, emails);
-  };
-
-  const onExport = async (id) => {
+  const onExport = async (id)=>{
     showLoader();
-    let projects = await exportProject(id)
+    let projects =  await exportProject(id)
     hideLoader();
   }
-
-  const onPullData = async () => {
-    showLoader();
-    await PullNewData(id);
-    hideLoader();
-  };
-
-
-  const onDownload = async (id) => {
-
-    let download = await downloadProject(id)
-    console.log(download)
-    if (download.status == 200) {
-      setData(download.data)
-
-    }
-
-  }
-
-
-  const exportData = () => {
-    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
-      JSON.stringify(data)
-    )}`;
-    const link = document.createElement("a");
-    link.href = jsonString;
-    link.download = "data.json";
-
-    link.click();
-  };
-  const handleChange = (e) => {
-    if (e === "CSV") {
-      setOptions(e)
-
-    }
-    else {
-      setOptions(e)
-    }
-
-  }
+   
+    const onPullData = async () => {
+      showLoader();
+      await PullNewData(id);
+      hideLoader();
+    };
+  
 
   const onEditProjectForm = async (values) => {
     showLoader();
@@ -174,104 +130,61 @@ function ProjectSettings() {
                 </Button>
               </Form.Item>
             </Form>
-            <Title level={3}>Add Annotators To The Project</Title>
-            <Form
-              name="basic"
-              layout="vertical"
-              onFinish={onFinishAddAnnotator}
-              autoComplete="off"
-            >
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "start",
-                  width: "100%",
-                }}
-              >
-                <Form.Item
-                  label="Emails"
-                  name="emails"
-                  rules={[
-                    {
-                      message: "Please enter annotator email",
-                    },
-                  ]}
-                  className="full-textarea"
-                  required="true"
-                >
-                  <TextArea
-                    rows={4}
-                    placeholder="Enter emails of Annotators separated by commas(,)"
-                  />
-                </Form.Item>
-
-                <div style={{ display: "flex" }}>
-                  <Form.Item
-                    wrapperCol={{ span: 16 }}
-                    style={{ marginRight: "10px" }}
-                  >
-                    <Button type="primary" htmlType="submit">
-                      Add Annotators
-                    </Button>
-                  </Form.Item>
-                  <Button type="primary" disabled={published} onClick={handlePublishProject}>
-                    Publish Project
-                  </Button>
-                </div>
-                <Title level={3}> Advanced Operation</Title>
-
-                <div style={{ display: "flex" }}>
-
-                  <Form.Item
-                    wrapperCol={{ span: 16 }}
-                    style={{ marginRight: "10px" }}
-                  >
-                    <Button type="primary" onClick={() => onExport(id)}>
-                      Export Project into Dataset
-                    </Button>
-                  </Form.Item>
-
-                  <Button type="primary" onClick={() => onPullData(id)}>
-                    Pull New Data Items from Source Dataset
-                  </Button>
-
-                  <Form.Item
-                    wrapperCol={{ span: 16 }}
-                  >
-                    <Select
-                      defaultValue="CSV"
-                      style={{
-                        width: 120,
-                        marginLeft: "10px"
-                      }}
-                      onChange={handleChange}
-                    >
-                      <Option value="CSV">CSV</Option>
-                      <Option value="JSON">JSON</Option>
-
-                    </Select>
-                  </Form.Item>
-                  <Form.Item
-                    wrapperCol={{ span: 16 }}
-                  >
-                    <Button type="primary" onClick={() => onDownload(id)}>
-                      Download project
-
-                    </Button>
-                  </Form.Item>
-                  {data?.length && (options == "CSV" ? <CSVDownload
-                    filename={"Expense_Table.csv"}
-                    data={data}
-                    target="_blank"
-                  >
-
-                  </CSVDownload> : exportData())
-
-                  }
-                </div>
+            <Title level={3}>Advanced Operations</Title>
+            <div style={{width:'50%', display:'flex', justifyContent:'space-between', marginBottom: '24px'}}>
+              <Button type="primary" disabled={published} onClick={handlePublishProject}>
+                Publish Project
+              </Button>
+              <Button type="primary"  onClick={() =>onExport(id)}>
+                Export project
+              </Button>
+              <Button type="primary" onClick={()=>onPullData(id)}>
+                Pull DataItems
+              </Button> 
+            </div>
+            <Title level={3}>Read-only Configurations</Title>
+            {project.sampling_mode && (
+              <div>
+                <Title level={4}>Sampling Parameters</Title>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className="sampling-params">
+                  <Col xs={12} md={8}>
+                    <p>Sampling Mode:</p>
+                  </Col>
+                  <Col xs={12} md={16}>
+                    <p>
+                      {project.sampling_mode == "b" && "Batch"}
+                      {project.sampling_mode == "r" && "Random"}
+                      {project.sampling_mode == "f" && "Full"}
+                    </p>
+                  </Col>
+                </Row>
+                {Object.keys(project.sampling_parameters_json).map((key, i) => (
+                  <Row key={i} gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className="sampling-params">
+                    <Col xs={12} md={8}>
+                      <p>{snakeToTitleCase(key)}:</p>
+                    </Col>
+                    <Col xs={12} md={16}>
+                      <p>{project.sampling_parameters_json[key]}</p>
+                    </Col>
+                  </Row>
+                ))}
               </div>
-            </Form>
+            )}
+            {Object.keys(project.variable_parameters).length !==0 && (
+              <div>
+              <Title level={4}>Variable Parameters</Title>
+              {Object.keys(project.variable_parameters).map((key, i) => (
+                  <Row key={i} gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className="variable-params">
+                    <Col xs={12} md={8}>
+                      <p>{snakeToTitleCase(key)}:</p>
+                    </Col>
+                    <Col xs={12} md={16}>
+                      <p>{project.variable_parameters[key]}</p>
+                    </Col>
+                  </Row>
+                ))}
+              </div>
+            )}
           </Card>
         </Col>
         <Col span={1} />
